8f83a3ae4ab9bd3028be8823df594b445b584c43
monikasmilga <tuuli705@gmail.com>
2017-08-21 15:28:04 +0300

    Edited ui

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css"
           integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
@@ -14,9 +15,15 @@
 <body>
 
 <div id="outer">
-    <h1>Fast typing game</h1>
+    <h1>Fast typing game <br>
+        <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+        <i class="fa fa-child" aria-hidden="true"></i>
+        <i class="fa fa-keyboard-o" aria-hidden="true"> </i>
+        <i class="fa fa-laptop" aria-hidden="true"></i>
+    </h1>
     <div id="register" class="invisible"><div class="greeting">
-        <h3>Welcome to fast typing game!</h3>
+
+            <h3>Welcome to fast typing game!</h3>
         <h4>Are you ready to play?</h4></div>
         <div class="input-group">
             <span class="input-group-addon" id="basic-addon1">Enter name</span>
@@ -27,7 +34,7 @@
 
         </div>
         <div>
-            Random letters will appear on the screen. Your goal is to type correctly as many letter as you can. One correct letter gives you 1 point. Golden letters give 5 points. Each 20 points you get 1 extra life. If you misstype, you loose 1 life. Good luck!
+            Random letters will appear on the screen. Your goal is to type correctly as many letters as you can. One correct letter gives you 1 point. Golden letters give 5 points. Each 20 points you get 1 extra life. If you misstype, you loose 1 life. Good luck!
         </div>
     </div>
 
